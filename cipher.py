1107a29ff7b14b10f549bb07f7c0ace070e6e3a8
soncreux <98056199+soncreux@users.noreply.github.com>
2023-09-09 10:44:04 +0300

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,28 @@
+def encrypt(text, shift):
+
+	result = ""
+
+
+	for  i in range(len(text)):
+
+		char = text[i]
+
+		if char==" ":
+			result+=" "
+
+		elif(char.isupper()):
+			result += chr((ord(char) + shift - 64) % 26 + 65)
+		else:
+			result += chr((ord(char) + shift - 96) % 26 + 97)
+	
+	return result
+
+
+text = input("Write your text: ")
+str_shift = input("Write shift: ")
+shift = int(str_shift)
+
+
+print("Plain text: " + text)
+print("Shift pattern: " + str(shift))
+print("Cipher: " + encrypt(text, shift))
